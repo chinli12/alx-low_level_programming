8fa0642c71605223cd36d3d7d5f7a813f4ef53b3
chinli12 <nolly889@gmail.com>
2023-02-28 02:55:41 -0800

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,4 +7,17 @@
  */
 void rev_string(char *s)
 {
+	char rv = s[0];
+	int counter = 0;
+	int i;
 
+	while (s[counter] != '\0')
+	counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rv = s[i];
+		s[i] = s[counter];
+		s[counter] = rv;
+	}
+}
