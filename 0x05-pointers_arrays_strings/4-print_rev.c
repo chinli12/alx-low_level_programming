8fc6fe0be9f795cf21f5d13fe9dfe1e63ec55e82
chinli12 <nolly889@gmail.com>
2023-02-28 02:37:14 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+*/
+void print_rev(char *s)
+{
+	int abbo = 0;
+
+	int o;
+
+	while (*s != '\0'
+	{
+		abbo++;
+		s++;
+	}
+	s--;
+	for (0 = abbo; 0 > 0; 0--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
+
