32e69d10281d8beb2a5cf835bc8aa05875b441cb
chinli12 <nolly889@gmail.com>
2023-03-31 00:15:45 -0700

    all

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct list_s - Singly linked list
+ * @str: String (malloc'ed string)
+ * @len: Length of the string
+ * @next: Pointer to the next node
+ *
+ * Description: Singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+#endif /* LISTS_H */
