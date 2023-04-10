cd1596c0502444cfd544a9278d5135d9a9570999
chinli12 <nolly889@gmail.com>
2023-04-09 23:54:26 -0700

    now

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,32 @@
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "main.h"
 
-int create_file(const char *filename, char *text_content)
-{
-    int fd, bytes_written, length = 0;
+int create_file(const char *filename, char *text_content) {
+  int fd, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+  if (filename == NULL) {
+    return -1;
+  }
 
-    if (text_content != NULL)
-    {
-        while (text_content[length])
-            length++;
+  if (text_content != NULL) {
+    while (text_content[len]) {
+      len++;
     }
+  }
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
+  fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+  if (fd == -1) {
+    return -1;
+  }
 
-    if (text_content != NULL)
-    {
-        bytes_written = write(fd, text_content, length);
-        if (bytes_written != length)
-        {
-            close(fd);
-            return (-1);
-        }
+  if (text_content != NULL) {
+    if (write(fd, text_content, len) == -1) {
+      close(fd);
+      return -1;
     }
+  }
 
-    close(fd);
-    return (1);
+  close(fd);
+  return 1;
 }
